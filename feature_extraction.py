cfbd22047f7ef21d3b978177abf35ebf10ccaedb
Saurabh Gupta <sg1705@gmail.com>
2016-12-31 09:38:19 -0500

    changed

--- feature_extraction.py
+++ feature_extraction.py
@@ -2,7 +2,8 @@ import pickle
 import tensorflow as tf
 from keras.layers import Input, Flatten, Dense
 from keras.models import Model
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -10,6 +11,8 @@ FLAGS = flags.FLAGS
 # command line flags
 flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
 flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
+flags.DEFINE_integer('epochs', 50, "The number of epochs.")
+flags.DEFINE_integer('batch_size', 256, "The batch size.")
 
 
 def load_bottleneck_data(training_file, validation_file):
@@ -39,9 +42,41 @@ def load_bottleneck_data(training_file, validation_file):
 def main(_):
     # load bottleneck data
     X_train, y_train, X_val, y_val = load_bottleneck_data(FLAGS.training_file, FLAGS.validation_file)
-
     print(X_train.shape, y_train.shape)
     print(X_val.shape, y_val.shape)
+
+    nb_classes = len(np.unique(y_train))
+    print(nb_classes)
+
+
+    # define model
+    input_shape = X_train.shape[1:]
+    inp = Input(shape=input_shape)
+    x = Flatten()(inp)
+    x = Dense(nb_classes, activation='softmax')(x)
+    model = Model(inp, x)
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    # train model
+    model.fit(X_train, y_train, nb_epoch=FLAGS.epochs, batch_size=FLAGS.batch_size, validation_data=(X_val, y_val), shuffle=True)
+
+
+
+    # Visualizations will be shown in the notebook.
+    # % matplotlib
+    # inline
+
+    # print(X_train[1][0][0])
+    # n_classes = len(np.unique(y_train))
+    # print(n_classes)
+    # print(np.squeeze(y_train).shape)
+    # fig, ax = plt.subplots()
+    # ax.bar(range(n_classes), np.bincount(np.squeeze(y_train)), 0.5, color='b')
+    # ax.set_xlabel('Sign')
+    # ax.set_ylabel('Count of Each Sign')
+    # ax.set_title('Histogram')
+    # plt.show()
+
 
     # TODO: define your model and hyperparams here
     # make sure to adjust the number of classes based on
